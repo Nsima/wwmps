4ad00880cafd4a3325551702f14fbb664ad0cbcf
Nsima <georgensima@gmail.com>
2025-06-21 15:15:18 +0100

    Fix:Vercel Analytics

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 import Chatbot from '@/components/Chatbot';
+import { Analytics } from "@vercel/analytics/next"
 import Head from 'next/head';
 export default function Page() {
   <Head>
         <title>What would my Pastor say?</title>
+  <Analytics />
   </Head>
   return <Chatbot />;
 }
